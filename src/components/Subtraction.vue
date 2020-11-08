51a7f0bd209c6197a7c73381520aae8558350ad0
twilight-down <www.12550.com@qq.com>
2020-11-08 15:56:43 +0800

    初始化计数器项目结构

---
+++ src/components/Subtraction.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <p>当前最新的Count值为:</p>
+  <button>-1</button>
+</div>
+</template>
+
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
